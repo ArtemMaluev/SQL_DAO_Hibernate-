81ae3c094352a5ece7fa7348c069285dc93eb544
ArtemMaluev <artem.rus13@yandex.ru>
2022-10-15 19:20:04 +0300

    Initial commit

---
+++ src/main/java/maluevartem/dao_layer_hibernate/DaoLayerHibernateApplication.java
@@ -0,0 +1,13 @@
+package maluevartem.dao_layer_hibernate;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DaoLayerHibernateApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DaoLayerHibernateApplication.class, args);
+    }
+
+}
